747e9062299b994be4a089d1b488ebff209d2d51
Anthony Li <li.anthony917@gmail.com>
2020-01-04 15:30:41 -0500

    Add other classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFoundationOpMode.kt
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.firstinspires.ftc.teamcode.movement.Arm
+import org.firstinspires.ftc.teamcode.movement.FoundationClamps
+import org.firstinspires.ftc.teamcode.util.AutoBaseOpMode
+import org.firstinspires.ftc.teamcode.util.drive
+
+class AutoFoundationOpMode(private val colorModifier: Double): AutoBaseOpMode(MILLISECONDS_PER_INCH) {
+    override fun runOpMode() {
+        super.runOpMode()
+
+        val arm = Arm.standard(hardwareMap)
+        val clamps = FoundationClamps.standard(hardwareMap)
+
+        waitForStart()
+
+        // Move clamps up
+        (clamps.move)(1.0, 500L)
+
+        // Move arm up
+        arm.setVerticalPower(1.0)
+        sleep(1000L)
+        arm.stop()
+
+        // Move forward one tile
+        drive(0, 1, 24)
+
+        // Move right one tile
+        drive(-colorModifier, 0.0, 24.0)
+
+        // Move forward 26 inches
+        drive(0, 1, 26)
+
+        // Move clamps down
+        (clamps.move)(-1.0, 500L)
+
+        // Move backwards 50 inches
+        drive(0, -1, 50)
+
+        // Move left 36 inches
+        drive(colorModifier, 1.0, 36.0)
+
+        // Move forward 1 inch
+        drive(0, 1, 1)
+
+        // Move arm down
+        arm.setVerticalPower(-1.0)
+        sleep(1000L)
+        arm.stop()
+
+        // Move left 36 inches
+        drive(colorModifier, 1.0, 36.0)
+    }
+}
+
+@Autonomous(name = "Foundation - Red", group = "Foundation")
+class AutoFoundationRedOpMode: AutoQuarryOpMode(1.0)
+
+@Autonomous(name = "Foundation - Blue", group = "Foundation")
+class AutoFoundationBlueOpMode: AutoQuarryOpMode(-1.0)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutoBaseOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutoBaseOpMode.kt
@@ -3,20 +3,46 @@ package org.firstinspires.ftc.teamcode.util
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.movement.MecanumDrive
 
-open class AutoBaseOpMode(val msPerInch: Double): LinearOpMode() {
+/**
+ * Boilerplate linear op mode for autonomous programs.
+ */
+open class AutoBaseOpMode(
+        /** Number of milliseconds to move the motors to drive one inch. **/
+        val msPerInch: Double
+): LinearOpMode() {
+    /**
+     * The robot's mecanum drive.
+     * @see MecanumDrive
+     */
     val drive: MecanumDrive by lazy { MecanumDrive.standard(hardwareMap) }
 
+    /**
+     * Contains code to run during the op mode. Override this method to implement your own subclass of AutoBaseOpMode.
+     *
+     * When overriding, ensure that you include a call to `super.runOpMode()`.
+     */
     override fun runOpMode() {
         drive
     }
 }
 
+/**
+ * Drives the motors at a specified direction for a specified number of inches.
+ * @param x horizontal movement.
+ * @param y vertical movement.
+ * @param inches number of inches to move and wait for.
+ */
 fun AutoBaseOpMode.drive(x: Double, y: Double, inches: Double) {
     drive.move(1.0, MecanumDrive.Motor.Vector2D(x, y), 0.0)
     sleep((msPerInch * inches).toLong())
     drive.stop()
 }
 
+/**
+ * Overload for the drive extension function.
+ *
+ * @see drive
+ */
 fun AutoBaseOpMode.drive(x: Int, y: Int, inches: Int) {
     drive(x.toDouble(), y.toDouble(), inches.toDouble())
 }
