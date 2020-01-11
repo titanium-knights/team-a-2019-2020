922e08cce3f4bb680b8f5373ae8a888f5756cfc6
Anthony Li <li.anthony917@gmail.com>
2020-01-10 19:35:56 -0500

    Add controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.kt
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Gamepad
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.teamcode.movement.Arm
 import org.firstinspires.ftc.teamcode.movement.FoundationClamps
 import org.firstinspires.ftc.teamcode.movement.Grabber
@@ -36,20 +38,69 @@ class TeleOpMode: OpMode() {
 
     private val standardSensors by lazy { StandardSensors(hardwareMap) }
     private val armDistance by lazy { standardSensors.armDistanceSensor }
+    private val armTouch by lazy { standardSensors.armTouchSensor }
 
     private var buttons = listOf<Button>()
     private val clampDownButton by lazy { makeButton(gamepad1, Gamepad::left_bumper)!! }
     private val clampUpButton by lazy { makeButton(gamepad1, Gamepad::right_bumper)!! }
     private val grabberDownButton by lazy { makeButton(gamepad2, Gamepad::left_bumper)!! }
     private val grabberUpButton by lazy { makeButton(gamepad2, Gamepad::right_bumper)!! }
+    private val moveArmDownButton by lazy { makeButton(gamepad2, Gamepad::dpad_down)!! }
+
+    private var armDownStartTime: Double? = null
+
+    private val elapsedTime by lazy { ElapsedTime() }
 
     override fun init() {
         // Since we're using lazy properties, get each of them to initialize them
-        drive; arm; clamps; grabber; armDistance
-        buttons = listOf(clampDownButton, clampUpButton, grabberDownButton, grabberUpButton)
+        drive; arm; clamps; grabber; armDistance; armTouch; elapsedTime
+        buttons = listOf(clampDownButton, clampUpButton, grabberDownButton, grabberUpButton, moveArmDownButton)
     }
 
     override fun loop() {
         buttons.forEach { it.update() }
+        val bypass = gamepad2.y
+
+        // Move mecanum drive
+        val vector = MecanumDrive.Motor.Vector2D(
+                -gamepad1.left_stick_y.toDouble(),
+                gamepad1.left_stick_x.toDouble()
+        )
+        val turn = gamepad1.right_stick_x.toDouble()
+        drive.move(1.0, vector, turn)
+
+        // Move clamps
+        if (clampDownButton.wasPressed()) {
+            clamps.moveDown()
+        }
+        if (clampUpButton.wasPressed()) {
+            clamps.moveUp()
+        }
+
+        // Move grabber
+        if (grabberDownButton.wasPressed()) {
+            grabber.grab()
+        }
+        if (grabberUpButton.wasPressed()) {
+            grabber.lift()
+        }
+
+        // Toggle whether arm is being moved down
+        if (moveArmDownButton.wasPressed()) {
+            armDownStartTime = if (armDownStartTime == null) elapsedTime.seconds() else null
+        }
+
+        // If arm is <= 3 cm from ground, or if we've spent over 5 seconds moving the arm, stop moving the arm down
+        if (
+                armDownStartTime != null &&
+                (armDistance.getDistance(DistanceUnit.INCH) <= 3 || elapsedTime.seconds() - armDownStartTime!! > 5)
+        ) {
+            armDownStartTime = null
+        }
+
+        // Move arm
+        val horizontal = gamepad2.left_stick_x.toDouble()
+        val vertical = if (armDownStartTime != null) -1.0 else -gamepad2.left_stick_y.toDouble()
+        arm.setPowers(horizontal, vertical)
     }
 }
