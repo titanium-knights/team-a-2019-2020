b29a75f834ae55586fde9b0b2606e7d517b27ac4
Anthony Li <li.anthony917@gmail.com>
2020-01-24 18:20:27 -0500

    Add back distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/StandardSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/StandardSensors.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.sensors;
 
-import android.graphics.Color;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -38,23 +37,18 @@ public class StandardSensors {
      * Returns a color sensor preconfigured for our robot.
      * @return a preconfigured color sensor.
      */
-    @Deprecated public ColorSensor getColorSensor() {
-        return getLeftColorSensor();
-    }
-    public ColorSensor getLeftColorSensor() {
-        return hardwareMap.get(ColorSensor.class, "left_color");
-    }
-    public ColorSensor getRightColorSensor() {
-        return hardwareMap.get(ColorSensor.class, "right_color");
+    public ColorSensor getColorSensor() {
+        return hardwareMap.get(ColorSensor.class, "color");
     }
 
     private DistanceSensor getDistanceSensor(String name) {
         return hardwareMap.get(DistanceSensor.class, name);
     }
 
-    public DistanceSensor getFrontDistanceSensor() { return getDistanceSensor("front_distance"); }
+    @Deprecated public DistanceSensor getFrontDistanceSensor() { return getDistanceSensor("front_distance"); }
     public DistanceSensor getLeftDistanceSensor() { return getDistanceSensor("left_distance"); }
     public DistanceSensor getRightDistanceSensor() { return getDistanceSensor("right_distance"); }
+    public DistanceSensor getBackDistanceSensor() { return getDistanceSensor("back_distance"); }
     public DistanceSensor getArmDistanceSensor() { return getDistanceSensor("arm_distance"); }
 
     public TouchSensor getArmTouchSensor() { return hardwareMap.get(TouchSensor.class, "arm_touch"); }
