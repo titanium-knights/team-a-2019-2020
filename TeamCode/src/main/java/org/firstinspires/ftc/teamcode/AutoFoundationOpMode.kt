3eda1056b39dee68e9a40dd5054015668f04376e
Anthony Li <li.anthony917@gmail.com>
2020-01-24 18:32:11 -0500

    Revamp AutoFoundationOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFoundationOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFoundationOpMode.kt
@@ -1,20 +1,26 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.hardware.DistanceSensor
 import org.firstinspires.ftc.teamcode.movement.FoundationClamps
 import org.firstinspires.ftc.teamcode.movement.Grabber
+import org.firstinspires.ftc.teamcode.sensors.StandardSensors
 import org.firstinspires.ftc.teamcode.util.*
-import kotlin.math.abs
-import kotlin.math.sign
 
 const val CLAMP_TIME = 1000L
 
-open class AutoFoundationOpMode(private val colorModifier: Double): AutoBaseOpMode(MILLISECONDS_PER_INCH) {
+open class AutoFoundationOpMode(
+        private val colorModifier: Double,
+        private val getSideSensor: StandardSensors.() -> DistanceSensor
+): AutoBaseOpMode(MILLISECONDS_PER_INCH) {
     override fun runOpMode() {
         super.runOpMode()
 
         val clamps = FoundationClamps.standard(hardwareMap)
         val grabber = Grabber.standard(hardwareMap)
+
+        val backDistance = standardSensors.backDistanceSensor
+        val sideDistance = standardSensors.run { getSideSensor() }
 
         waitForStart()
 
@@ -23,38 +29,26 @@ open class AutoFoundationOpMode(private val colorModifier: Double): AutoBaseOpMo
         // Move clamps up
         clamps.moveUp()
         grabber.lift()
+        raiseArm()
 
-        // Move left 0.5 tile
-        drive(-colorModifier, 0.0, 12.0)
-
-        // Move forward 1.25 tiles
-        drive(0, 1, 33)
-
-        // Move clamps down
+        // Move forward to foundation
+        drive(Vector2D(0.0, 1.0), startingDir, backDistance, -33.0)
         clamps.moveDown()
         sleep(CLAMP_TIME)
 
-        // Move backwards 1.5 tiles
-        drive(0, -1, 37)
+        drive(Vector2D(0.0, -1.0), startingDir, backDistance, 2.0)
 
-        // Move clamps up
         clamps.moveUp()
         sleep(CLAMP_TIME)
 
-        turn(startingDir)
-
         drive(0.0, 0.5, 2.0)
 
-        // Move right 36 inches
-        drive(colorModifier, 0.0, 36.0)
-
-        // Move right 30 inches
-        drive(colorModifier, 0.0, 24.0)
+        drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, 63.0)
     }
 }
 
 @Autonomous(name = "Route 2 - Red", group = "Foundation")
-class AutoFoundationRedOpMode: AutoFoundationOpMode(-1.0)
+class AutoFoundationRedOpMode: AutoFoundationOpMode(-1.0, StandardSensors::getRightDistanceSensor)
 
 @Autonomous(name = "Route 2 - Blue", group = "Foundation")
-class AutoFoundationBlueOpMode: AutoFoundationOpMode(1.0)
+class AutoFoundationBlueOpMode: AutoFoundationOpMode(1.0, StandardSensors::getLeftDistanceSensor)
