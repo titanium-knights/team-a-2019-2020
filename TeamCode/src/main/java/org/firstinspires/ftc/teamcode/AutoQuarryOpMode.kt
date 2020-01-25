b37bead65747755c85a54c6f533f70e841160e9f
Anthony Li <li.anthony917@gmail.com>
2020-01-25 12:50:05 -0500

    Finish skystone collection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoQuarryOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoQuarryOpMode.kt
@@ -36,9 +36,35 @@ open class AutoQuarryOpMode(
         DistanceSide.RIGHT -> standardSensors.leftDistanceSensor
     } }
 
-    private var skystonePos = 0 // 0 is stone at center
+    private var skystonePos = 0 // 0 is stone at wall
 
     private val grabberTime = 1000L
+
+    fun collect(startingDir: Double) {
+        drive(Vector2D(0.0, 1.0), startingDir, backDistance, -35.0)
+        grabber.grab()
+        sleep(grabberTime)
+        raiseArm()
+
+        // Push back
+        drive(Vector2D(0.0, -1.0), startingDir, backDistance, 25.2)
+
+        // Cross skybridge
+        drive(Vector2D(-colorModifier, 0.0), startingDir, sideDistance, -50.0, false)
+        drive(Vector2D(-colorModifier, 0.0), startingDir, otherSideDistance, 50.0)
+
+        // Release stone
+        arm.setVerticalPower(1.0)
+        grabber.lift()
+        sleep(700L)
+        arm.stop()
+        grabber.grab()
+
+        drive(colorModifier * 0.5, 0.0, 15.0)
+        arm.setVerticalPower(-1.0)
+        sleep(700L)
+        arm.stop()
+    }
 
     override fun runOpMode() {
         super.runOpMode()
@@ -59,6 +85,7 @@ open class AutoQuarryOpMode(
         // Lift grabber and disable color sensor LED
         grabber.lift()
         clamps.moveDown()
+        drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, 30.0)
 
         // Move towards the center stone
         drive(Vector2D(0.0, 1.0), startingDir, backDistance, -31.0)
@@ -71,44 +98,29 @@ open class AutoQuarryOpMode(
 
         skystonePos = when {
             abs(left - right) < 0.04 -> 1
-            left < right -> 0
-            else -> 2
+            left < right -> 2
+            else -> 0
         }
 
         when (skystonePos) {
-            0 -> drive(Vector2D(-colorModifier, 0.0), startingDir, sideDistance, -40.0)
-            2 -> drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, 21.0)
+            2 -> drive(Vector2D(-colorModifier, 0.0), startingDir, sideDistance, -40.0)
+            0 -> drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, 21.0)
         }
 
-        drive(Vector2D(0.0, 1.0), startingDir, backDistance, -35.0)
-        grabber.grab()
-        sleep(grabberTime)
-        raiseArm()
+        skystonePos = sign(colorModifier).toInt() * (skystonePos - 1) + 1
 
-        // Push back
-        drive(Vector2D(0.0, -1.0), startingDir, backDistance, 24.0)
+        collect(startingDir)
 
-        // Cross skybridge
-        drive(Vector2D(-colorModifier, 0.0), startingDir, otherSideDistance, 50.0)
-
-        // Release stone
-        arm.setVerticalPower(1.0)
-        grabber.lift()
-        sleep(500L)
-        arm.stop()
-        grabber.grab()
-
-        return
-
-        drive(Vector2D(colorModifier, 0.0), startingDir, otherSideDistance, -55.0)
-        arm.setVerticalPower(-1.0)
-        sleep(500L)
         arm.setPowers(0.5, 0.0)
         sleep(200L)
         arm.setHorizontalPower(0.0)
         lowerArm()
 
-        drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, (if (skystonePos == 2) 5 else skystonePos) * 8.0 - 5)
+        drive(Vector2D(colorModifier, 0.0), startingDir, sideDistance, (if (skystonePos == 0) 5 else skystonePos) * 8.0 - 1.5)
+        grabber.lift()
+
+        collect(startingDir)
+        drive(colorModifier * 0.5, 0.0, 15.0)
     }
 }
 
